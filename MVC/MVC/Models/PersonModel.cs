c21bf899b034ba4ebc3b75e1aca4914f9813d73e
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-04-03 22:10:28 +0300

    add first MVC

---
+++ MVC/MVC/Models/PersonModel.cs
@@ -0,0 +1,22 @@
+﻿namespace MVC.Models
+{
+    public class PersonModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public PersonModel()
+        {
+            Id = Guid.NewGuid();
+
+        }
+
+        public PersonModel(string name, DateTime dateOfBirth) : this()
+        {
+            name = Name;
+            dateOfBirth = DateOfBirth;
+        }
+    }
+}
